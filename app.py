f7768620dab71ff9fe7ddb4e74680b5c18eb30d6
Andy Gates <agates01@hotmail.com>
2020-05-24 22:35:04 +0100

    db

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-PROBLEMS = [
+import os
+import psycopg2
+import json
+
+DATABASE_URL = 'postgresql://postgres:password@localhost:5432/boardbook' #os.getenv('DATABASE_URL')
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    image = db.Column(db.String())
+    holds = db.Column(db.Text())
+
+    def __init__ (self, name, image, holds):
+        self.name = name
+        self.image = image
+        self.holds = holds
+
+class Problem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)
+    name = db.Column(db.String())
+    grade = db.Column(db.String())
+    holds = db.Column(db.ARRAY(db.Integer))
+    
+    def __init__ (self, board_id, name, grade, holds):
+        self.board_id = board_id
+        self.name = name
+        self.grade = grade
+        self.holds = holds
+
+Problems = [
     {
         'name'  : 'Problem 1',
         'grade' : '6A',
@@ -19,17 +60,8 @@ PROBLEMS = [
     }
 ]
 
-BOARD = { 
-    'image': "board.png",
-    'holds': []
-}
-
-# configuration
-DEBUG = True
-
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
+conn = psycopg2.connect(DATABASE_URL, sslmode='prefer')
+print("DB CONNECTION: " + DATABASE_URL)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
@@ -38,32 +70,47 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def getProblems():
     return jsonify({
         'status': 'success',
-        'problems': PROBLEMS
+        'problems': Problems
     })
 
 @app.route('/board', methods=['GET'])
 def getBoardData():
+    return jsonify({
+        'status': 'success',
+        'board': addTestBoard()
+    })
 
-    genBoard = BOARD
-    genBoard['holds'] = genHolds()
+@app.route('/test', methods=['POST'])
+def test():
+    
+    b = addTestBoard()
+    bobj = Board(b['name'], b['image'], jsonify(b['holds']))
+
+    db.session.add(bobj)
+    db.session.commit()
+
+    print(bobj.id)
 
     return jsonify({
         'status': 'success',
-        'board': genBoard
     })
 
-def genHolds():
-    holds = []
-    for x in range(1, 10):
-        for y in range(1,10):
-            holds.append(
-                {
-                    'x'     : 0.1 * x,
-                    'y'     : 0.1 * y,
-                    'size'  : 20.0
-                }
-            )
-    return holds
+
+def loadHolds(path):
+    with open(path) as json_file:
+        data = json.load(json_file)
+        return data['holds']
+
+def addTestBoard():
+    DataPath = os.path.join(os.path.dirname(__file__), 'data.json')
+    
+    Board = { 
+        'name' : "Home Board",
+        'image': "board.png",
+        'holds': loadHolds(DataPath)
+    }
+
+    return Board
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')
